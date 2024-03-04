93fd9aedecba5d311aeab43a399b9b1971d67492
7dgonzalez <115291267+7dgonzalez@users.noreply.github.com>
2024-03-04 15:58:24 -0400

    Add files via upload

---
+++ project.py
@@ -0,0 +1,26 @@
+import unittest
+from vector import Vector
+
+class TestVectors(unittest.TestCase):
+    def setUp(self):
+        self.v1 = Vector(1.0, -2.0, -2.0)
+        self.v2 = Vector(3.0, 6.0, 9.0)
+
+    def test_magnitude(self):
+        self.assertEqual(self.v1.magnitude(), 3)
+
+    def test_addition(self):
+        sum = self.v1 + self.v2
+        self.assertEqual(getattr(sum, "x"), 4.0)
+
+    def test_multiplication(self):
+        mul = self.v1 * 2
+        self.assertEqual(getattr(mul, "x"), 2.0)
+
+    def test_division(self):
+        div = self.v1 / 2
+        self.assertEqual(getattr(div, "x"), 0.5)
+
+if __name__ == '__main__':
+    unittest.main()
+
